26c020828d2d7d500ed3c7d5c9f3051f160d71bb
garaco <ale17garaco@gmail.com>
2021-01-28 03:15:19 -0600

    compilacion con cordova

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // api_uri:"https://api.comunidadseytu.com/express_api/public/api",
-  api_uri:"http://127.0.0.1:8000/api",
+  api_uri:"https://api.comunidadseytu.com/express_api/public/api",
+  // api_uri:"http://127.0.0.1:8000/api",
   Authorization: "Base UXVldHphbGNvYXRsLjIxO0FKR0EjMTdzZlRIcmR7",
   AppId: "App-Express-Api"
 };
